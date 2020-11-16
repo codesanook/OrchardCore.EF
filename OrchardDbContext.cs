9f889c61381f41887b2321e325ab9b0de87b9c07
aaronamm <theeranitp@gmail.com>
2020-11-16 13:25:38 +0700

    Set ConfigureAwait(false) to prevent dead lock in blocking async method

--- OrchardDbContext.cs
+++ OrchardDbContext.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using OrchardCore.Environment.Extensions;
 using OrchardCore.Environment.Shell;
 
 namespace OrchardCore.EF
@@ -22,21 +25,29 @@ namespace OrchardCore.EF
             this.logger = logger;
         }
 
-        protected override async void OnModelCreating(ModelBuilder modelBuilder)
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             try
             {
-                var enableExtensions = await shellFeaturesManager.GetEnabledExtensionsAsync();
-                var enableModuls = enableExtensions.Where(x => x.Manifest.Type.ToUpperInvariant() == "MODULE");
-                var assemblies = enableModuls.Select(m => Assembly.Load(m.Id));
-                foreach (var assembly in assemblies)
-                {
-                    modelBuilder.ApplyConfigurationsFromAssembly(assembly);
-                }
+                // Use GetAwaiter().GetResult() to propagate an exception rather than wrapping them in an AggregateException.
+                SetMappingsAsync(modelBuilder).GetAwaiter().GetResult();
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, "Error in OrchardContext.OnModelCreating");
+            }
+        }
+
+        private async Task SetMappingsAsync(ModelBuilder modelBuilder)
+        {
+            // Set ConfigureAwait(false) to complete a method after a task is complete without resuming a web context. 
+            // https://blog.stephencleary.com/2012/07/dont-block-on-async-code.html#preventing-the-deadlock
+            var enableExtensions = await shellFeaturesManager.GetEnabledExtensionsAsync().ConfigureAwait(false);
+            var enableModuls = enableExtensions.Where(x => x.Manifest.Type.ToUpperInvariant() == "MODULE");
+            var assemblies = enableModuls.Select(m => Assembly.Load(m.Id));
+            foreach (var assembly in assemblies)
+            {
+                modelBuilder.ApplyConfigurationsFromAssembly(assembly);
             }
         }
     }
